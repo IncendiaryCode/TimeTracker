473395beabf371e640972d3ddba9c7ca924e561a
vinay-km <56333718+vinay-km@users.noreply.github.com>
2019-11-08 11:04:13 +0530

    ui for graph.

--- user/employee_activities.php
+++ user/employee_activities.php
@@ -6,29 +6,69 @@ include("header.php");
 <main class="container-fluid container-fluid-main">
     <div class="main-container container">
         <div class="main-container-inner mt-3">
-                <div class="au-card au-card--no-shadow au-card--no-pad mb-2">
-                    <div class="au-card-title pt-5">
-                        <div class="bg-overlay bg-overlay--blue"></div>
-                        <h3>
-                            <i class="zmdi zmdi-account-calendar"></i>
-                        </h3>
-                    </div>
-                    <div class="au-task js-list-load">
-                        <div class="au-task-list js-scrollbar3">
-                            <div class="au-task__item au-task__item--danger">
-                                <div class="row au-task__item-inner attach  m-1" id="attachPanels" >
-                                <!-- Loading in js-->
+            <div class="au-card au-card--no-shadow au-card--no-pad mb-2">
+                <div class="au-card-title pt-5">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Daily activitie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Weekly activities</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Monthly activities</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="daily-chart">
+                                <div class="form-group">
+                                    <label for="end-date">Select date</label>
+                                    <div class="input-group">
+                                        <!-- chart that shows daily activities -->
+                                        <input type="date" class="form-control" id="daily-chart">
+                                        <button class="btn btn-primary" onclick="dailyChart()">view chart</button>
+                                    </div>
                                 </div>
+                                <canvas id="daily"></canvas>
                             </div>
-                            
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="daily-chart">
+                                <div class="form-group">
+                                    <label for="end-date">Select week</label>
+                                    <div class="input-group">
+                                        <input type="week" class="form-control " id="weekly-chart">
+                                        <button class="btn btn-primary" onclick="weeklyChart()">view chart</button>
+                                    </div>
+                                </div>
+                                <canvas id="weekly"></canvas>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                            <div class="p-5" id="calendar_basic">
+                            </div>
+                            <div class="clear">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <hr>
-            <footer>
-                <p class="text-center p-3 ">Copyright © 2019 Printgreener.com</p>
-            </footer>
+            <div class="au-task js-list-load">
+                <div class="au-task-list js-scrollbar3">
+                    <div class="au-task__item au-task__item--danger">
+                        <div class="row au-task__item-inner attach  m-1" id="attachPanels">
+                            <!-- Loading in js-->
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </main>
-    <?php include("footer.php"); ?>
+    </div>
+    <hr>
+    <footer>
+        <p class="text-center p-3 ">Copyright © 2019 Printgreener.com</p>
+    </footer>
+    </div>
+</main>
+<?php include("footer.php"); ?>
--- user/monthly_details.php
+++ user/monthly_details.php
@@ -5,11 +5,11 @@ include("header.php");
 <main class="container-fluid container-fluid-main">
     <div class="main-container container">
         <div class="main-container-inner">
-            <div>
                 <!-- Green theme -->
-               <div class="auto-jsCalendar material-theme green" id="calendar" ></div>
+               <div class="p-5" id="calendar_basic" >
+               </div>
+                <div class="clear">
             </div>
-            <div class="clear"></div>
         </div>
         <hr class="mt-5">
         <footer>
