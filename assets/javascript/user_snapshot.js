0be85c3eba21309e4f09d75eac85545f512fec84
vinay-km <56333718+vinay-km@users.noreply.github.com>
2019-12-13 13:27:11 +0530

    user-deatils in admin panel.
    - chart for user details in admin panel in integrated

--- assets/javascript/user_snapshot.js
+++ assets/javascript/user_snapshot.js
@@ -16,7 +16,7 @@ var user_task_name = [];
 var task_value = res['result'][0];
 var user_value = res['result'][1];
 
-console.log("user_value", user_value, Object.keys(user_value).length, Object.keys(user_value)[0], Object.values(user_value), Object.values(user_value)[0].length, );
+console.log("user_value", task_value, Object.keys(user_value).length, Object.keys(user_value)[0], Object.values(user_value), Object.values(user_value)[0].length, );
 for(var i=0; i<task_value.length; i++)
 {
 task_labels[i] = task_value[i]['task_name'];
@@ -33,63 +33,27 @@ var task_array = {
         data: task_time_value
     };
 user_data[0] = task_array;
-
-/*for user*/
-var c=0;
-var cnt=0;
 for(var j=0; j<Object.keys(user_value).length; j++)
 {
     user_labels = Object.keys(user_value)[j];
     for(var k=0; k<Object.values(user_value)[0].length; k++)
     {
-    console.log(Object.values(user_value)[0][k][1])
-        user_time_value[k] = Object.values(user_value)[0][k][1];
+        user_time_value[[k]] = Object.values(user_value)[0][k][1]/60;
     }
-
-console.log(user_labels, user_time_value );
     var inner_array = { 
         type: 'line',
-        //label: user_value[j][0],
+        label: user_labels,
         backgroundColor:color(window.chartColors.red).alpha(0.5).rgbString(),
         borderColor:window.chartColors.green,
         fill: false,
-        data: /*user_value[j]['time_used']/60*/user_time_value[j],
-
+        data: user_time_value,
         };
        user_data[j+1] = inner_array;
 }
 
 var chart_values = {
     labels: task_labels,
-    datasets: user_data 
-    /*{
-        type: 'bar',
-        label: 'Total tasks',
-        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.red,
-        data: task_time_value,
-    }, {
-        type: 'line',
-        label: 'user 1',
-        backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.green,
-        fill: false,
-        data: [5, 100, 20, 40, 10, 20],
-    }, {
-        type: 'line',
-        label: 'user 2',
-        backgroundColor: "red",
-        borderColor: window.chartColors.orange,
-        fill: false,
-        data: [40, 20, 50, 10, 300, 50],
-    }, {
-        type: 'line',
-        label: 'user 3',
-        backgroundColor: "red",
-        borderColor: window.chartColors.blue,
-        fill: false,
-        data: [0, 11, 20, 25, 60, 20],
-    }*/
+    datasets: user_data
 };
 var configs = {
     type: 'bar',
