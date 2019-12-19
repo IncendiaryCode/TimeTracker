ccaab7af5fa6f14a9293d735752e7f0d6ba93afb
vinay-km <56333718+vinay-km@users.noreply.github.com>
2019-12-19 17:04:24 +0530

    -project details UI in admin panel.

--- application/views/project_snapshot.php
+++ application/views/project_snapshot.php
@@ -32,69 +32,49 @@ $picture = substr($profile,29);
     <p class="display-4 text-primary text-center">Project details</p>
         <div class="container">
             <div class="row mt-5">
-                <div class="col-md-8 offset-md-2">
+                <div class="col-12">
                     <!-- <canvas id="project-chart"></canvas> -->
-                    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+                    <div id="chart_div" style=" height: 500px;"></div>
                 </div>
             </div>
             <div class="row mt-5">
-                <div class="col-4">
+                <div class="col-5">
                     <p><strong>Project name</strong></p>
                 </div>
-                <div class="col-4">
+                <div class="col-5">
                     <p><strong>Users</strong></p>
                 </div>
                 <div class="col-2">
                     <p><strong>Time spent</strong></p>
                 </div>
-                <div class="col-2">
-                    <p><strong>Actions</strong></p>
-                </div>
             </div>
             <hr>
                 <?php foreach($data as $proj){ 
                     ?>
-            <div class="row">
-                <div class="col-4">
-                    <div>
+            <div class="row" style="min-height: 50px;">
+                <div class="col-5">
 
-                            <a href="#" class="badge">
-                            <div class="row badge-light project-name">
-                                <div class="col-2"><img src=""></div>
-                                <div class="col-8"><?= $proj['project_name']; ?></div>
-                            </div>
+                    <div class=" project-name">
+                        <a href="#" class="">
+                            <div class=""><img src=""><?= $proj['project_name']; ?></div>
                         </a>
                     </div>
                     
                 </div>
-                <div class="col-4">
+                <div class="col-5">
                     <p>Total users: <?=$proj['total_users'];?></p>
                      <?php foreach($proj['user_details'] as $user){  ?>
-                         <a href="#" class="badge badge-light pt-2 mt-2">
+                         <a href="#" class="pt-2 mr-3 mt-2">
                             <?= $user['user_name']; ?>
                         </a>
                     <?php  }?>
                 </div>
-                <div class="col-2"><?=round($proj['time_used']/60,2)?></div>
-                <div class="col-2"><i class="fas fa-trash-alt icon-plus project-remove text-danger" data-toggle="modal" data-target="#delete-project" ><input type="hidden" name=""  class="project_id" value="<?=$proj['project_id'];?>"></i></div>
+                <div class="col-2"><?=round($proj['time_used']/60,2)?> hrs</div>
+                
             </div><hr>
                 <?php  } ?>
         </div>
 
-    <div class="modal" id="delete-project" data-backdrop="false">
-        <div class="modal-dialog">
-            <div class="modal-content text-center">
-                <div class="modal-header ">
-                    <span>Do you want to delete? </span>
-                    <button type="button" class="close text-danger" data-dismiss="modal">×</button>
-                </div>
-                    <div class="modal-footer text-center">
-                        <button type="button" class="btn btn-primary card22" id="cancel-delete" data-dismiss="modal">No</button>
-                        <button type="button" class="btn btn-primary card22" id="delete-proj" >Yes</button>
-                    </div>
-            </div>
-        </div>
-    </div>
     <hr>
     <footer>
         <p class="text-center">Copyright © 2019 Printgreener.com</p>
